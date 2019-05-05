b1e1c5d25066cb51aea18cee0cf25e1e5c553feb
checkshumm <checkshumm@gmail.com>
2019-05-04 20:30:48 -0400

    Refactored list task view to a hash map with unique key

--- lib/src/blocs/task_bloc.dart
+++ lib/src/blocs/task_bloc.dart
@@ -1,25 +1,40 @@
 import 'dart:async';
 import 'dart:developer';
 
+import 'package:flutter/material.dart';
 import 'package:pine/src/data/task.dart';
 import 'package:rxdart/rxdart.dart';
 
 class TaskBloc {
-  var _tasks = <Task>[];
-  final _taskSubject = BehaviorSubject<List<Task>>();
+  var _tasks = new Map<Key, Task>();
+  final _taskSubject = BehaviorSubject<Map<Key, Task>>();
 
   getSubject() {
     return _taskSubject;
   }
 
-  getTasks() async {
-    _taskSubject.sink.add(_tasks);
+  getTasks() {
+    // _taskSubject.sink.add(_tasks);
+    return _tasks;
   }
 
   updateTasks(title, description, type) {
-    this._tasks.add(
+    this._tasks[new UniqueKey()] = (
         new Task(id: _tasks.length, title: title, description: description, type: type));
     _taskSubject.sink.add(this._tasks);
+    //this.dispose();
+  }
+
+  setTaskColor(int id, Color color) {
+    _tasks[id].setColor(color);
+  }
+
+  swap(Key draggedItem, Key newPosition) {
+    Task swappedTask = _tasks[draggedItem];
+    this._tasks[draggedItem] = this._tasks[newPosition];
+    this._tasks[newPosition] = swappedTask;
+    _taskSubject.sink.add(this._tasks);
+    //this.dispose();
   }
 
   dispose() {
--- lib/src/data/task.dart
+++ lib/src/data/task.dart
@@ -1,8 +1,16 @@
+import 'package:flutter/material.dart';
+
 class Task {
+
   int id;
   String title;
   String description;
   String type;
+  Color color;
 
-  Task({this.id, this.title, this.description, this.type});
+  Task({this.id, this.title, this.description, this.type, this.color});
+
+  setColor(Color color) {
+    this.color = color;
+  }
 }
